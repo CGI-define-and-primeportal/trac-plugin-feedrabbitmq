271d266595100674642f7fdcc2e530c4258b2366
Nick Piper <nick.piper@cgi.com>
2015-09-18 10:02:53 +0100

    Refactor out the actual event pushing from the listeners, ready to have optional listeners

--- tracfeedrabbitmq/listeners.py
+++ tracfeedrabbitmq/listeners.py
@@ -18,14 +18,32 @@ from itertools import chain
 # TODO invent IHoursListener (Trachours plugin)
 # TODO admin ui to configure project_identifier and turn on active
 
-class Listener(Component):
-    implements(ITicketChangeListener,
-               IAttachmentChangeListener)
+class QueueFeeder(Component):
 
     amqp = Option("amqp", "broker", default="amqp://guest:guest@localhost//")
     project_identifier = Option("amqp", "project_identifer")
     queue_names = ListOption("amqp", "queues", default="ticket_event_feed")
     active = BoolOption("amqp", "active", default=False)    
+
+    def send_events(self, events):
+        # TODO should we actually be creating Connection() and queue in __init__?
+        # TODO What can we do if we can't connect? Buffer in our own database?
+        if not self.active:
+            return
+        self.log.debug("Connecting to %s with queues %s", self.amqp, self.queue_names)
+        with Connection(self.amqp) as conn:
+            queues = [conn.SimpleQueue(queue_name, queue_opts={'durable': True})
+                      for queue_name in self.queue_names]
+            for event in events:
+                event['_project'] = self.project_identifier or os.path.basename(self.env.path)
+                for queue in queues:
+                    self.log.debug("Putting event %s to queue %s", event, queue_name)
+                    queue.put(event, serializer="yaml")
+        
+
+class TicketListener(Component):
+    implements(ITicketChangeListener,
+               IAttachmentChangeListener)
 
     def ticket_created(self, ticket):
         event = {k: self._transform_value(k, ticket[k]) for k in ticket.values}
@@ -35,7 +53,7 @@ class Listener(Component):
                       "_time": _time,
                       "_ticket": ticket.id,
                       "_author": ticket['reporter']})
-        self._send_events([event])
+        QueueFeeder(self.env).send_events([event])
             
     def ticket_changed(self, ticket, comment, author, old_values):
         _time = datetime.datetime.utcnow()
@@ -49,7 +67,7 @@ class Listener(Component):
             comment_event = []
         # produces one event for each item in old_values, plus
         # possibly the comment
-        self._send_events(chain(comment_event,
+        QueueFeeder(self.env).send_events(chain(comment_event,
                                 ({"_category": "changed",
                                   "_time": _time,
                                   "_ticket": ticket.id,
@@ -62,7 +80,7 @@ class Listener(Component):
         event = {"_category": "deleted",
                  "_time": _time,
                  "_ticket": ticket.id}
-        self._send_events([event])
+        QueueFeeder(self.env).send_events([event])
 
     def ticket_comment_modified(self, ticket, cdate, author, comment, old_comment):
         _time = datetime.datetime.utcnow()        
@@ -72,7 +90,7 @@ class Listener(Component):
                  "_author": author,
                  "_cdate": cdate,                 
                  "comment": comment}
-        self._send_events([event])
+        QueueFeeder(self.env).send_events([event])
 
     def ticket_change_deleted(self, ticket, cdate, changes):
         # we don't support this, as the authors of this plugin don't
@@ -90,7 +108,7 @@ class Listener(Component):
                  "filename": attachment.filename,
                  "description": attachment.description,
                  "size": attachment.size}
-        self._send_events([event])
+        QueueFeeder(self.env).send_events([event])
 
     def attachment_deleted(self, attachment):
         _time = datetime.datetime.utcnow()
@@ -101,7 +119,7 @@ class Listener(Component):
                  "_ticket": attachment.parent_realm,
                  "_author": attachment.author,                 
                  "filename": attachment.filename}
-        self._send_events([event])
+        QueueFeeder(self.env).send_events([event])
 
     def attachment_version_deleted(self, attachment, old_version):
         """Called when a particular version of an attachment is deleted."""
@@ -119,19 +137,3 @@ class Listener(Component):
         # TODO deal with integer, date, float fields (CGI extensions)
         # TODO ensure that 'changetime' is in UTC?
         return value
-
-    def _send_events(self, events):
-        # TODO should we actually be creating Connection() and queue in __init__?
-        # TODO What can we do if we can't connect? Buffer in our own database?
-        if not self.active:
-            return
-        self.log.debug("Connecting to %s with queues %s", self.amqp, self.queue_names)
-        with Connection(self.amqp) as conn:
-            queues = [conn.SimpleQueue(queue_name, queue_opts={'durable': True})
-                      for queue_name in self.queue_names]
-            for event in events:
-                event['_project'] = self.project_identifier or os.path.basename(self.env.path)
-                for queue in queues:
-                    self.log.debug("Putting event %s to queue %s", event, queue_name)
-                    queue.put(event, serializer="yaml")
-        
